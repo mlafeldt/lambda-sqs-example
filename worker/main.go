e607504f3365482a0651ada49ab3d00cbcb0ce0b
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2018-07-18 11:21:03 +0200

    Check that the function is invoked correctly

--- worker/main.go
+++ worker/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
@@ -13,8 +14,13 @@ func main() {
 }
 
 func handler(ctx context.Context, sqsEvent events.SQSEvent) error {
+	if len(sqsEvent.Records) == 0 {
+		return errors.New("No SQS message passed to function")
+	}
+
 	for _, msg := range sqsEvent.Records {
 		fmt.Printf("Got SQS message %s with body: %s\n", msg.MessageId, msg.Body)
 	}
+
 	return nil
 }

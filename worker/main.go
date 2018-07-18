d89db255a0b3268ce7d3ade0f8652035f710292e
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2018-07-18 11:36:15 +0200

    Tweak output format

--- worker/main.go
+++ worker/main.go
@@ -19,7 +19,7 @@ func handler(ctx context.Context, sqsEvent events.SQSEvent) error {
 	}
 
 	for _, msg := range sqsEvent.Records {
-		fmt.Printf("Got SQS message %s with body: %s\n", msg.MessageId, msg.Body)
+		fmt.Printf("Got SQS message %q with body %q\n", msg.MessageId, msg.Body)
 	}
 
 	return nil
